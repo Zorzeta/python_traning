d6a2ab5ab55fee3450c18d3dba90cdd9ab86109d
olgak <olga.kaminska@op.pl>
2018-05-24 21:30:28 +0200

    fixtura

--- test_add_group.py
+++ test_add_group.py
@@ -7,18 +7,18 @@ from application import Application
 
 @pytest.fixture()
 def app(request):
-    fixture = Application
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="test", header="test1", footer="test"))
-        app.logout()
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="test", header="test1", footer="test"))
+    app.logout()
 
 
 def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
