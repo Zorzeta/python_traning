9e0e90c9aa8bfc3547b64d9877a6fb2afa3971f2
olgak <olga.kaminska@op.pl>
2018-05-24 16:34:19 +0200

    test2_zapisany_recorderem

---
+++ groupcontact.py
@@ -0,0 +1,8 @@
+class Group:
+
+    def __init__(self, name, lastname, nickname, phone, email):
+        self.name = name
+        self.lastname = lastname
+        self.nickname = nickname
+        self.phone = phone
+        self.email = email
